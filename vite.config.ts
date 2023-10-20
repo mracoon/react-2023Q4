4e8cb9c4aff4149f3d56ce3b6c024ff81b46f4ec
mracoon <nataliyakomarova777@gmail.com>
2023-10-20 10:54:03 +0300

    feat: upd test config (coverage)

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,18 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
-    // you might want to disable it, if you don't have tests that rely on CSS
-    // since parsing CSS is slow
-    css: true,
+    css: false,
+    coverage: {
+      all: true,
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['**/*.tsx'],
+    },
   },
 });
