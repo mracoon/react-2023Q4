e2b9572490335f8d17da80a39973cbe9eb8a3626
mracoon <nataliyakomarova777@gmail.com>
2023-10-25 12:18:30 +0300

    feat: abort multiple request

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, Component, KeyboardEvent } from 'react';
 import { DataType, RequestItem } from '../../types/apiDataTypes';
 import { ApiErrorMessage } from '../Error/ApiErrorMessage';
 import { AiOutlineSearch } from 'react-icons/ai';
+import ResponsiveBtn from '../buttons/ResponsiveBtn';
 
 interface ISearchProps {
   change: (data: RequestItem[]) => void;
@@ -13,53 +14,62 @@ interface ISearchState {
   data: RequestItem[];
   hasApiErr: boolean;
   errMessage?: string;
-  isLoading: boolean;
 }
 
 export default class SearchBar extends Component<ISearchProps, ISearchState> {
+  controller: AbortController;
   constructor(props: ISearchProps) {
     super(props);
     this.state = {
       val: localStorage.getItem('mracoon-search-query') ?? '',
       data: [],
       hasApiErr: false,
-      isLoading: false,
     };
     this.handlerChange = this.handlerChange.bind(this);
     this.handlerEnter = this.handlerEnter.bind(this);
     this.submitHandler = this.submitHandler.bind(this);
+    this.controller = new AbortController();
   }
 
-  handlerChange(e: ChangeEvent<HTMLInputElement>) {
-    this.setState({ val: e.target.value });
-  }
-
-  submitHandler() {
-    localStorage.setItem('mracoon-search-query', this.state.val.trim());
-    this.setState({ hasApiErr: false, isLoading: true });
-    this.props.loading(true);
+  getSearchData(page = 1, limit = 6) {
     fetch(
-      `https://api.jikan.moe/v4/anime?page=1&sfw&limit=6&${
+      `https://api.jikan.moe/v4/anime?page=${page}&sfw&limit=${limit}${
         this.state.val ? '&q=' + this.state.val : ''
-      }`
+      }`,
+      { signal: this.controller.signal }
     )
       .then((res) => {
         return res.json();
       })
       .then((data: DataType) => {
         this.props.change(data.data);
+        this.props.loading(false);
       })
       .catch((err: Error) => {
+        const isAbortErr = err.name === 'AbortError';
+        this.props.change([]);
+        this.props.loading(isAbortErr);
+        if (isAbortErr) {
+          return;
+        }
         this.setState({
           hasApiErr: true,
           errMessage: `${err.name}: ${err.message}`,
         });
-        this.props.change([]);
-      })
-      .finally(() => {
-        this.props.loading(false);
-        this.setState({ isLoading: false });
       });
+  }
+
+  handlerChange(e: ChangeEvent<HTMLInputElement>) {
+    this.setState({ val: e.target.value });
+  }
+
+  async submitHandler() {
+    this.controller.abort();
+    this.controller = new AbortController();
+    localStorage.setItem('mracoon-search-query', this.state.val.trim());
+    this.setState({ hasApiErr: false });
+    this.props.loading(true);
+    this.getSearchData();
   }
 
   componentDidMount(): void {
@@ -78,23 +88,21 @@ export default class SearchBar extends Component<ISearchProps, ISearchState> {
       <>
         <div className=" w-full flex gap-2 justify-between items-center">
           <input
-            className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 disabled:cursor-wait"
+            className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
             type="search"
             placeholder="Search anime"
             onChange={this.handlerChange}
             onKeyUp={this.handlerEnter}
             value={this.state.val}
-            disabled={this.state.isLoading}
           />
-          <button
-            className="btn bg-indigo-600 disabled:cursor-wait"
-            disabled={this.state.isLoading}
-            onClick={this.submitHandler.bind(this)}
-          >
-            <AiOutlineSearch className="block sm:hidden" />
-            <span className="hidden sm:block">Search</span>
-          </button>
+          <ResponsiveBtn
+            classes="bg-indigo-600"
+            text="Search"
+            icon={AiOutlineSearch}
+            onClickHandler={this.submitHandler.bind(this)}
+          />
         </div>
+
         {this.state.hasApiErr && (
           <ApiErrorMessage message={this.state.errMessage ?? ''} />
         )}
