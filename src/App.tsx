1464ce056357f3944223165e2bd026dcf9768066
mracoon <nataliyakomarova777@gmail.com>
2023-10-26 17:27:47 +0300

    feat: migrate App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Component } from 'react';
 import './App.css';
 import SearchPage from './components/SearchPage';
 import ErrorBoundary from './components/Error/ErrorBoundary';
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <ErrorBoundary>
-          <SearchPage></SearchPage>
-        </ErrorBoundary>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <ErrorBoundary>
+        <SearchPage></SearchPage>
+      </ErrorBoundary>
+    </>
+  );
+};
+
+export default App;
