3d6db336e3fd42b7897ec31c2d8eea7c872cb914
mracoon <nataliyakomarova777@gmail.com>
2023-10-26 17:23:13 +0300

    feat: migrate ResultsContainer

--- src/components/ResultsContainer.tsx
+++ src/components/ResultsContainer.tsx
@@ -1,23 +1,65 @@
-import { Component } from 'react';
-import Card from './Card/Card';
-import { RequestItem } from '../types/apiDataTypes';
+import { useEffect, useState } from 'react';
+import { DataType, RequestItem } from '../types/apiDataTypes';
+import { ApiErrorMessage } from './Error/ApiErrorMessage';
+import { CardsContainer } from './Card/CardsContainer';
 
+interface IApiErr {
+  hasApiErr: boolean;
+  errMessage?: string;
+}
 interface IResultsContainerProps {
-  cardsData: RequestItem[];
+  searchVal: string;
 }
+const ResultsContainer = ({ searchVal }: IResultsContainerProps) => {
+  const [cardsData, setCardsData] = useState<RequestItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [apierr, setApiErr] = useState<IApiErr>({ hasApiErr: false });
 
-export default class ResultsContainer extends Component<IResultsContainerProps> {
-  render() {
-    return (
-      <div className="flex flex-wrap gap-2 justify-center">
-        {this.props.cardsData.length ? (
-          this.props.cardsData.map((data) => (
-            <Card data={data} key={data.mal_id}></Card>
-          ))
-        ) : (
-          <h2>no results</h2>
-        )}
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    setIsLoading(true);
+    setApiErr({ hasApiErr: false });
+    const ctrl = new AbortController();
+    fetch(
+      `https://api.jikan.moe/v4/anime?page=1&sfw&limit=6${'&q=' + searchVal}`,
+      {
+        signal: ctrl.signal,
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data: DataType) => {
+        setCardsData(data.data);
+        setIsLoading(false);
+      })
+      .catch((err: Error) => {
+        const isAbortErr = err.name === 'AbortError';
+        setCardsData([]);
+        setIsLoading(isAbortErr);
+        if (isAbortErr) {
+          return;
+        }
+        setApiErr({
+          hasApiErr: true,
+          errMessage: `${err.name}: ${err.message}`,
+        });
+      });
+    return () => {
+      ctrl.abort();
+    };
+  }, [searchVal]);
+
+  return (
+    <div className="flex flex-wrap flex-col gap-2 items-center w-full">
+      {isLoading && <p className="loader"></p>}
+      {apierr.hasApiErr && (
+        <ApiErrorMessage message={apierr.errMessage ?? ''} />
+      )}
+
+      {!isLoading && !apierr.hasApiErr && (
+        <CardsContainer cardsData={cardsData}></CardsContainer>
+      )}
+    </div>
+  );
+};
+export default ResultsContainer;
