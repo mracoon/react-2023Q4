980b302fb5de9a3ec7765a1d609d51da3933f12a
mracoon <nataliyakomarova777@gmail.com>
2023-10-30 19:14:29 +0300

    refactor: create updatePage func

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -2,25 +2,26 @@ import { useSearchParams } from 'react-router-dom';
 import { ReqPagination } from '../../types/apiDataTypes';
 
 export const Pagination = ({ pagInfo }: { pagInfo: ReqPagination }) => {
-  const page = +(
-    localStorage.getItem('mracoon-pag-page') ?? pagInfo.current_page
-  );
+  const page = pagInfo.current_page;
   const [, setSearchParams] = useSearchParams({
     page: `${pagInfo.current_page}`,
   });
 
   const { last_visible_page: lastPage, has_next_page: hasNextPage } = pagInfo;
 
-  const increment = () => {
-    const newPage = Math.min(page + 1, lastPage);
+  const updatePage = (newPage: number) => {
     setSearchParams({ page: `${newPage}` });
     localStorage.setItem('mracoon-pag-page', `${newPage}`);
   };
 
+  const increment = () => {
+    const newPage = Math.min(page + 1, lastPage);
+    updatePage(newPage);
+  };
+
   const decrement = () => {
     const newPage = Math.max(page - 1, 0);
-    setSearchParams({ page: `${newPage}` });
-    localStorage.setItem('mracoon-pag-page', `${newPage}`);
+    updatePage(newPage);
   };
 
   return (
