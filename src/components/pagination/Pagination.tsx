d0c4f72232c0c26a3ee8cbea0b5660113162fa34
mracoon <nataliyakomarova777@gmail.com>
2023-10-30 16:03:24 +0300

    feat: upd Pagination component

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,30 +1,37 @@
 import { useSearchParams } from 'react-router-dom';
+import { ReqPagination } from '../../types/apiDataTypes';
 
-export const Pagination = ({
-  pageChange,
-}: {
-  pageChange: (page: number) => void;
-}) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const page = +(searchParams.get('page') ?? '1');
+export const Pagination = ({ pagInfo }: { pagInfo: ReqPagination }) => {
+  const page = +(
+    localStorage.getItem('mracoon-pag-page') ?? pagInfo.current_page
+  );
+  const [, setSearchParams] = useSearchParams({
+    page: `${pagInfo.current_page}`,
+  });
+
+  const { last_visible_page: lastPage, has_next_page: hasNextPage } = pagInfo;
 
   const increment = () => {
-    const newPage = page + 1;
-    pageChange(newPage);
+    const newPage = Math.min(page + 1, lastPage);
     setSearchParams({ page: `${newPage}` });
+    localStorage.setItem('mracoon-pag-page', `${newPage}`);
   };
 
   const decrement = () => {
-    const newPage = page - 1;
-    pageChange(newPage);
+    const newPage = Math.max(page - 1, 0);
     setSearchParams({ page: `${newPage}` });
+    localStorage.setItem('mracoon-pag-page', `${newPage}`);
   };
 
   return (
     <div className="flex-center gap-4">
-      <button onClick={decrement}>prev</button>
+      <button onClick={decrement} disabled={page === 1 || undefined}>
+        prev
+      </button>
       <p style={{ background: 'white' }}>{page}</p>
-      <button onClick={increment}>next</button>
+      <button onClick={increment} disabled={!hasNextPage || undefined}>
+        next
+      </button>
     </div>
   );
 };
