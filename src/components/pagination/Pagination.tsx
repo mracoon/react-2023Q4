4c75f4cc4b0cb887c31c60930ce542d92d587a65
mracoon <nataliyakomarova777@gmail.com>
2023-11-03 19:50:00 +0300

    refactor: refactor pagination component

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,12 +1,18 @@
 import { useSearchParams } from 'react-router-dom';
-import { ReqPagination } from '../../types/apiDataTypes';
-export const Pagination = ({ pagInfo }: { pagInfo: ReqPagination }) => {
-  const page = pagInfo.current_page;
+import { RequestPagination } from '../../types/apiDataTypes';
+
+interface IPaginationInfoProps {
+  paginationInfo: RequestPagination;
+}
+
+export const Pagination = ({ paginationInfo }: IPaginationInfoProps) => {
+  const page = paginationInfo.current_page;
   const [, setSearchParams] = useSearchParams({
-    page: `${pagInfo.current_page}`,
+    page: `${paginationInfo.current_page}`,
   });
 
-  const { last_visible_page: lastPage, has_next_page: hasNextPage } = pagInfo;
+  const { last_visible_page: lastPage, has_next_page: hasNextPage } =
+    paginationInfo;
 
   const updatePage = (newPage: number) => {
     setSearchParams({ page: `${newPage}` });
---
+++ src/test/paginationTemplate.ts
@@ -0,0 +1,10 @@
+export const paginationTemplate = {
+  last_visible_page: 1,
+  has_next_page: false,
+  current_page: 1,
+  items: {
+    count: 0,
+    total: 0,
+    per_page: 0,
+  },
+};
