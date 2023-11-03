7784bc945aa034f91c62f247234683ab9e34b8c3
mracoon <nataliyakomarova777@gmail.com>
2023-11-03 11:11:25 +0300

    fix: use act & userEvent in tests

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SearchBar from './SearchBar';
 
 describe('SearchBar', () => {
@@ -26,16 +27,20 @@ describe('SearchBar', () => {
   });
 
   it('should render search bar', () => {
-    render(<SearchBar valChange={changeStub}></SearchBar>);
+    act(() => {
+      render(<SearchBar valChange={changeStub} />);
+    });
     expect(screen.getByRole('searchbox')).toBeInTheDocument();
   });
 
   it('should call the submitHandler when press Enter and save query in ls', async () => {
-    render(<SearchBar valChange={changeStub} />);
+    userEvent.setup();
+    act(() => {
+      render(<SearchBar valChange={changeStub} />);
+    });
     const searchInput = screen.getByRole<HTMLInputElement>('searchbox');
-    fireEvent.change(searchInput, { target: { value: 'test query string' } });
-    fireEvent.keyUp(searchInput, {
-      key: 'Enter',
+    await act(async () => {
+      await userEvent.type(searchInput, 'test query string{enter}');
     });
     await waitFor(() => expect(changeStub).toHaveBeenCalled());
     expect(storage['mracoon-search-query']).toBe('test query string');
