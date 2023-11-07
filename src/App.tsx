c336ba7e7bee07682e14d5ecd017fea706b540e2
mracoon <nataliyakomarova777@gmail.com>
2023-11-07 16:12:15 +0300

    feat: add hage 404

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ const App = () => {
               <Route path="/" element={<Details />}></Route>
             </Route>
           </Route>
+          <Route path="/*" element={<h1>Page not found</h1>}></Route>
         </Routes>
       </ErrorBoundary>
     </>
