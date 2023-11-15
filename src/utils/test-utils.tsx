e148f46b1a3ce2739ab2892891f7da83ca754584
mracoon <nataliyakomarova777@gmail.com>
2023-11-15 03:01:06 +0300

    feat: create renderWithProvider

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,30 @@
+import React, { PropsWithChildren } from 'react';
+import { render } from '@testing-library/react';
+import type { RenderOptions } from '@testing-library/react';
+import type { PreloadedState } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import { AppStore, RootState, setupStore } from '../store/store';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  setupListeners(store.dispatch);
+
+  function Wrapper({ children }: PropsWithChildren<unknown>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
