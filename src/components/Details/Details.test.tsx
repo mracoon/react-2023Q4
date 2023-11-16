294e1781e27d07cd26eec934adb1642bdb089506
mracoon <nataliyakomarova777@gmail.com>
2023-11-16 13:04:06 +0300

    fix: fix Details tests

--- src/components/Details/Details.test.tsx
+++ src/components/Details/Details.test.tsx
@@ -1,100 +1,78 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
 import { Details } from './Details';
 import { MemoryRouter } from 'react-router-dom';
 import { detailsMockData } from '../../test/detailsMockData';
 import userEvent from '@testing-library/user-event';
 import { Nullable } from '../../types/apiDataTypes';
-
-const { mockedMethod } = vi.hoisted(() => {
-  return {
-    mockedMethod: vi.fn(() => {
-      return new Promise((resolve) => resolve({ data: detailsMockData }));
-    }),
-  };
-});
-
-vi.mock('../../utils/API', () => {
-  return { getApiData: mockedMethod };
-});
-
-mockedMethod.mockImplementationOnce(() => {
-  return new Promise((resolve) =>
-    setTimeout(resolve, 1000, { data: detailsMockData })
-  );
-});
+import { renderWithProviders } from '../../utils/test-utils';
 
 describe('Details', () => {
   userEvent.setup();
-  let detailedCardId: Nullable<number> = 1;
-  const cardClickHandlerStub = vi.fn();
-  beforeEach(() => {
-    vi.clearAllMocks();
-  });
+  const detailedCardId: Nullable<string> = '123';
 
   it('a loading indicator should be displayed while fetching data', async () => {
     expect(screen.queryByTestId('loader')).toBeNull();
     await act(async () => {
-      render(
+      renderWithProviders(
         <MemoryRouter>
-          <Details detailCardId={1} cardClickHandler={cardClickHandlerStub} />
-        </MemoryRouter>
+          <Details />
+        </MemoryRouter>,
+        {
+          preloadedState: {
+            viewModeReducer: { page: 1, detailsId: detailedCardId },
+          },
+        }
       );
     });
+
     expect(screen.queryByTestId('loader')).not.toBeNull();
   });
 
   it('detailed card component should display the detailed card data correctly', async () => {
     await act(async () => {
-      render(
+      renderWithProviders(
         <MemoryRouter>
-          <Details detailCardId={1} cardClickHandler={cardClickHandlerStub} />
-        </MemoryRouter>
+          <Details />
+        </MemoryRouter>,
+        {
+          preloadedState: {
+            viewModeReducer: { page: 1, detailsId: detailedCardId },
+          },
+        }
       );
     });
 
     const title = detailsMockData.title_english || detailsMockData.title;
-    expect(screen.getByText(title)).toBeInTheDocument();
-    expect(screen.getByAltText(title)).toBeInTheDocument();
-    expect(
-      screen.getByText(detailsMockData.synopsis ?? 'no description')
-    ).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText(title)).toBeInTheDocument();
+      expect(screen.getByAltText(title)).toBeInTheDocument();
+      expect(
+        screen.getByText(detailsMockData.synopsis ?? 'no description')
+      ).toBeInTheDocument();
+    });
   });
 
   it('clicking the close button should hide the component', async () => {
-    cardClickHandlerStub.mockImplementation((id: Nullable<number>) => {
-      detailedCardId = id;
-    });
-    let container = document.body;
-    await act(async () => {
-      container = render(
+    const container = await act(async () => {
+      return renderWithProviders(
         <MemoryRouter>
-          <Details
-            detailCardId={detailedCardId}
-            cardClickHandler={cardClickHandlerStub}
-          />
-        </MemoryRouter>
+          <Details />
+        </MemoryRouter>,
+        {
+          preloadedState: {
+            viewModeReducer: { page: 1, detailsId: detailedCardId },
+          },
+        }
       ).container;
-    });
-
-    expect(container.getElementsByClassName('details').length).toBe(1);
-    await act(async () => {
-      await userEvent.click(screen.getByText('✖'));
     });
 
     await waitFor(async () => {
-      expect(cardClickHandlerStub).toBeCalledWith(null);
+      expect(container.getElementsByClassName('details').length).toBe(1);
+
+      await userEvent.click(screen.getByText('✖'));
+
+      expect(container.getElementsByClassName('details').length).toBe(0);
     });
-    await act(async () => {
-      container = render(
-        <MemoryRouter>
-          <Details
-            detailCardId={detailedCardId}
-            cardClickHandler={cardClickHandlerStub}
-          />
-        </MemoryRouter>
-      ).container;
-    });
-    expect(container.getElementsByClassName('details').length).toBe(0);
   });
 
   afterAll(() => {
