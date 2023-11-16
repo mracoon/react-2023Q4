99ead724f43caded7928eb167cf98f7806d34086
mracoon <nataliyakomarova777@gmail.com>
2023-11-16 13:03:33 +0300

    feat: setup msw

---
+++ src/mock/handler.ts
@@ -0,0 +1,18 @@
+import { HttpResponse, http } from 'msw';
+import { BASE_URL } from '../utils/constants';
+import { detailsMockData } from '../test/detailsMockData';
+import { mockData } from '../test/mockData';
+
+export const handlers = [
+  http.get(`${BASE_URL}/:id`, () => {
+    return HttpResponse.json({ data: detailsMockData });
+  }),
+
+  http.get(`${BASE_URL}`, () => {
+    return new Response(
+      JSON.stringify({
+        data: mockData,
+      })
+    );
+  }),
+];
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,11 @@
 import '@testing-library/jest-dom';
+import { server } from '../mock/server';
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
 
 afterAll(() => {
   vi.clearAllMocks();
+  server.close();
 });
