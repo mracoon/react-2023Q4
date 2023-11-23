03e120940dd93bda3c54259111b793de71cfcdd1
mracoon <nataliyakomarova777@gmail.com>
2023-11-23 15:20:18 +0300

    feat: create uncontrolled search input

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ChangeEvent, KeyboardEvent, useState, useEffect } from 'react';
+import React, { useRef } from 'react';
+import { KeyboardEvent, useEffect } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import ResponsiveBtn from '../buttons/ResponsiveButton';
 
@@ -9,26 +9,22 @@ const SearchBar = () => {
   const router = useRouter();
   const { query } = router;
   const { limit } = query;
-  const [value, setValue] = useState((query.searchValue || '').toString());
+  const searchInputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
-    setValue((router.query.searchValue || '').toString());
     const pageParam = router.query?.page;
     if (!pageParam) {
       router.push({ query: { ...router.query, page: 1 } });
     }
   }, [router]);
-  const handlerChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
 
   const submitHandler = () => {
-    const searchValue = value.trim();
+    const searchValue = searchInputRef.current?.value.trim() ?? '';
+    console.log(searchValue);
     router.push({ query: { page: 1, searchValue, limit: limit || 1 } });
   };
 
   const handlerEnter = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter' && event.target instanceof HTMLInputElement) {
-      setValue(event.target.value);
       submitHandler();
     }
   };
@@ -40,9 +36,9 @@ const SearchBar = () => {
           className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
           type="search"
           placeholder="Search anime"
-          onChange={handlerChange}
           onKeyUp={handlerEnter}
-          value={value}
+          ref={searchInputRef}
+          defaultValue={(router.query.searchValue || '').toString()}
         />
         <ResponsiveBtn
           classes="bg-indigo-600"
