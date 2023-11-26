5a9b3a5ff8e683c8606121616cacf93bd253dc74
mracoon <nataliyakomarova777@gmail.com>
2023-11-26 13:48:08 +0300

    feat: add countries select

--- src/components/Form/CountriesSelect.tsx
+++ src/components/Form/CountriesSelect.tsx
@@ -1,28 +1,66 @@
-import { CustomSelect } from './CustomSelect';
+import { ChangeEvent, useState } from 'react';
+import { countries } from '../../utils/countriesList';
 
-export const CountriesSelect = ({
-  inputRef,
+const CountriesSelect = ({
+  countriesRef,
+  errorMessage,
 }: {
-  inputRef: React.RefObject<HTMLSelectElement>;
+  countriesRef: React.RefObject<HTMLInputElement>;
+  errorMessage?: string;
 }) => {
+  const [inputValue, setInputValue] = useState('');
+  const [filteredCountries, setFilteredCountries] = useState<string[]>([]);
+
+  const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+    const filteredCountryList = countries.filter((country) =>
+      country.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredCountries(filteredCountryList);
+  };
+
+  const clickHandler = (countryOption: string) => {
+    setInputValue(countryOption);
+    setFilteredCountries([]);
+  };
+  const blurHandler = () => {
+    setTimeout(() => {
+      setFilteredCountries([]);
+    }, 100);
+  };
+
   return (
-    <CustomSelect
-      lableText="Countries"
-      name="countries"
-      inputId="countries"
-      inputRef={inputRef}
-      options={[
-        { value: 'ru', text: 'Russia' },
-        { value: 'by', text: 'Belarus' },
-        { value: 'usa', text: 'USA' },
-      ]}
-    ></CustomSelect>
+    <div className="flex flex-col items-start justify-start">
+      <div className="relative">
+        <label htmlFor="countries">Countries:</label>
+        <input
+          id="countries"
+          type="text"
+          value={inputValue}
+          name="country"
+          onChange={inputChangeHandler}
+          placeholder="Choose country..."
+          onBlur={blurHandler}
+          ref={countriesRef}
+          onFocus={inputChangeHandler}
+        />
+        {filteredCountries.length > 0 && (
+          <ul className="max-h-40 overflow-y-auto absolute bg-slate-100">
+            {filteredCountries.map((countryOption, index) => (
+              <li
+                key={`country-${index}`}
+                onClick={() => clickHandler(countryOption)}
+              >
+                {countryOption}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+    </div>
   );
 };
 
-/*        <label htmlFor="countries">Countries:</label>
-          <select name="countries" autoComplete="country" id="countries">
-            <option value="ru">Russia</option>
-            <option value="by">Belarus</option>
-            <option value="usa">USA</option>
-          </select> */
+export default CountriesSelect;
