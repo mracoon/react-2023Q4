c54d0453d07e277f2c0055a4460fde327646458f
mracoon <nataliyakomarova777@gmail.com>
2023-12-02 04:31:28 +0300

    feat: change to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { setupStore } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={setupStore()}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
